86803040c5efa30eab0ff94a350e2cf74a8d2e6a
Branning <bmk2110@gmail.com>
2022-12-16 16:46:11 -0500

    Restructured the JS because of inaccessable variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,78 @@
-// Assignment Code
+//Links generate password button to the generateBtn variable so we can add event listener
 var generateBtn = document.querySelector("#generate");
-var specialCharacters = confirm("Would you like special characters in your password?\nOK=Yes\nCancel=No")
-var passwordLength = prompt("How long would you like the password to be?", "Enter desired password length.")
-var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var specialChars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-console.log(passwordLength)
-console.log(specialCharacters)
+//generates the password to display to the user
+function generatePassword(){
+//Password length prompts
+    var passwordLength = prompt("How long would you like the password to be?", "Enter a number from 8 to 128.");
+    if(passwordLength < 8){
+        alert('Password length must be at least 8 characters')
+        return null;
+    }
+    if(passwordLength > 128){
+        alert('Password length must less than 129 characters')
+        return null;
+    }
+//Character Type Prompts
+    var upperChars = confirm("Would you like uppercase letters in your password?");
+    var lowerChars = confirm("Would you like lowercase letters in your password?");
+    var nums = confirm("Would you like numbers in your password?");
+    var specialChars = confirm("Would you like special characters in your password?");
+    
+//If no character types are selected
+    if (!upperChars && !lowerChars && !nums && !specialChars) {
+        alert("You must choose at least one character type");
+        console.log("You must choose at least one character type");
+        return;
+    }
 
-// function generatePassword(){
-//   for (var i = 0; i <= passwordLength; i++) {
-//     var randomNumber = Math.floor(Math.random() * passwordLength);
-//     password += chars.substring(randomNumber, randomNumber +1);
-//    } return;
-// }
-// console.log(generatePassword())
+//Character types to pull from
+    var uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var specialCharacters = '!@#$%^&*()';
+//Character types defined by user
+    var passwordCharacterList = '';
+    var potientialPassword = '';
+    var password = '';
 
-//maybe need to rewrite the function inside of the if else
-if (specialCharacters === true){
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    console.log(chars);
-    function generatePassword(){
-        for (var i = 0; i <= passwordLength; i++) {
-          var randomNumber = Math.floor(Math.random() * passwordLength);
-          var password = chars.substring(randomNumber, randomNumber +1);
-         }
-         console.log(password)
-      }
-    console.log(generatePassword())
-} else {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    console.log(chars);
-    function generatePassword(){
-        for (var i = 0; i <= passwordLength; i++) {
-          var randomNumber = Math.floor(Math.random() * passwordLength);
-          password = chars.substring(randomNumber, randomNumber);
-         }
-         console.log(password)
-      }
-    console.log(generatePassword())
+//Adds character types to potentialPassword string
+    if(upperChars == true){
+        potientialPassword += uppercaseLetters
+    }
+    if(lowerChars == true){
+        potientialPassword += lowercaseLetters
+    }
+    if(nums == true){
+        potientialPassword += numbers
+    }
+    if(specialChars == true){
+        potientialPassword += specialCharacters
+    }
+    console.log(potientialPassword)
+//Randomizes password
+    for(let i=0; i< Number(passwordLength); i++ ){
+        var randomNum = Math.floor(Math.random() * potientialPassword.length);
+        console.log(randomNum);
+        var randomChar = potientialPassword[randomNum];
+        console.log(randomChar);
+        passwordCharacterList = passwordCharacterList + randomChar;
+        console.log(passwordCharacterList);
+
+        password = passwordCharacterList
+    }
+
+return password
 }
 
-// Write password to the #password input
+//initiates the generate password function
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
